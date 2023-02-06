e49522c40dd14991901fa5ff5446b17a46423ed6
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2023-02-06 14:26:06 +0100

    Add poster

--- src/components/BackgroundVideo.jsx
+++ src/components/BackgroundVideo.jsx
@@ -3,10 +3,10 @@ export function BackgroundVideo() {
     <div>
       <video width="100%" autoPlay muted loop>
         <source
-          src="https://res.cloudinary.com/dt3k2apqd/video/upload/q_auto,f_auto/Loop%20Film/LoopFilm_01_n930vc.mp4"
+          src="https://res.cloudinary.com/dt3k2apqd/video/upload/q_auto/Loop%20Film/LoopFilm_01_n930vc.mp4"
           type="video/mp4"
+          poster="https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/Loop%20Film/bmw-large_rbr1c8.webp"
         />
-        Your browser does not support the video tag.
       </video>
     </div>
   )
