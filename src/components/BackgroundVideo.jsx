b2a8d50a2adea76e927281dc7d542504d6e95e32
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2023-02-06 14:35:29 +0100

    add extra video source

--- src/components/BackgroundVideo.jsx
+++ src/components/BackgroundVideo.jsx
@@ -1,11 +1,20 @@
 export function BackgroundVideo() {
   return (
     <div>
-      <video width="100%" autoPlay muted loop>
+      <video
+        width="100%"
+        autoPlay
+        muted
+        loop
+        poster="https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/Loop%20Film/bmw-large_rbr1c8.webp"
+      >
         <source
           src="https://res.cloudinary.com/dt3k2apqd/video/upload/q_auto/Loop%20Film/LoopFilm_01_n930vc.mp4"
           type="video/mp4"
-          poster="https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/Loop%20Film/bmw-large_rbr1c8.webp"
+        />
+        <source
+          src="https://res.cloudinary.com/dt3k2apqd/video/upload/c_scale,h_280/f_auto/Loop%20Film/LoopFilm_01_n930vc.mp4"
+          type="video/mp4"
         />
       </video>
     </div>
