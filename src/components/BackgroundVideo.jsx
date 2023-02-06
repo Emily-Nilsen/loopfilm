00b6123ce96ed3536129c6a63907768a6df5048e
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2023-02-06 14:12:42 +0100

    Add f auto to video

--- src/components/BackgroundVideo.jsx
+++ src/components/BackgroundVideo.jsx
@@ -3,9 +3,10 @@ export function BackgroundVideo() {
     <div>
       <video width="100%" autoPlay muted loop>
         <source
-          src="https://res.cloudinary.com/dt3k2apqd/video/upload/q_auto/Loop%20Film/LoopFilm_01_n930vc.mp4"
+          src="https://res.cloudinary.com/dt3k2apqd/video/upload/q_auto,f_auto/Loop%20Film/LoopFilm_01_n930vc.mp4"
           type="video/mp4"
         />
+        Your browser does not support the video tag.
       </video>
     </div>
   )
