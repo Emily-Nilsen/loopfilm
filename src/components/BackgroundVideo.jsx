366ed02fdea1e80bb35326a315b0f4aad9ac6efc
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2023-02-06 14:41:57 +0100

    Optimise video to webm format

--- src/components/BackgroundVideo.jsx
+++ src/components/BackgroundVideo.jsx
@@ -9,11 +9,7 @@ export function BackgroundVideo() {
         poster="https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/Loop%20Film/bmw-large_rbr1c8.webp"
       >
         <source
-          src="https://res.cloudinary.com/dt3k2apqd/video/upload/q_auto/Loop%20Film/LoopFilm_01_n930vc.mp4"
-          type="video/mp4"
-        />
-        <source
-          src="https://res.cloudinary.com/dt3k2apqd/video/upload/c_scale,h_280/f_auto/Loop%20Film/LoopFilm_01_n930vc.mp4"
+          src="https://res.cloudinary.com/dt3k2apqd/video/upload/f_webm/q_auto/Loop%20Film/LoopFilm_01_n930vc.mp4"
           type="video/mp4"
         />
       </video>
