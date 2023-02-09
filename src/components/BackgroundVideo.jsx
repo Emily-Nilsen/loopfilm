02d3e0d2790bb626f5eb1c8bc32f1f13fb2aedac
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2023-02-09 10:18:34 +0100

    Add mobile div with controls

--- src/components/BackgroundVideo.jsx
+++ src/components/BackgroundVideo.jsx
@@ -1,18 +1,35 @@
 export function BackgroundVideo() {
   return (
-    <div>
-      <video
-        width="100%"
-        autoPlay
-        muted
-        playsInline
-        // poster="https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/Loop%20Film/bmw-large_rbr1c8.webp"
-      >
-        <source
-          src="https://res.cloudinary.com/dt3k2apqd/video/upload/f_webm/Loop%20Film/LoopFilm_ix0zjg.mp4"
-          type="video/mp4"
-        />
-      </video>
-    </div>
+    <>
+      <div className="hidden sm:block">
+        <video
+          width="100%"
+          autoPlay
+          muted
+          playsInline
+          // poster="https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/Loop%20Film/bmw-large_rbr1c8.webp"
+        >
+          <source
+            src="https://res.cloudinary.com/dt3k2apqd/video/upload/f_webm/Loop%20Film/LoopFilm_ix0zjg.mp4"
+            type="video/mp4"
+          />
+        </video>
+      </div>
+      <div class="sm:hidden">
+        <video
+          width="100%"
+          autoPlay
+          muted
+          playsInline
+          controls
+          // poster="https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/Loop%20Film/bmw-large_rbr1c8.webp"
+        >
+          <source
+            src="https://res.cloudinary.com/dt3k2apqd/video/upload/f_webm/Loop%20Film/LoopFilm_ix0zjg.mp4"
+            type="video/mp4"
+          />
+        </video>
+      </div>
+    </>
   )
 }
