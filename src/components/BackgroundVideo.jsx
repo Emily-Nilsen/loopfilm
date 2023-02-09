4160d940fb80291efa0697c5439b69019abbd2ba
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2023-02-09 09:56:03 +0100

    Add new video

--- src/components/BackgroundVideo.jsx
+++ src/components/BackgroundVideo.jsx
@@ -5,11 +5,11 @@ export function BackgroundVideo() {
         width="100%"
         autoPlay
         muted
-        loop
+        // loop
         poster="https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/Loop%20Film/bmw-large_rbr1c8.webp"
       >
         <source
-          src="https://res.cloudinary.com/dt3k2apqd/video/upload/f_webm/q_auto/Loop%20Film/LoopFilm_01_n930vc.mp4"
+          src="https://res.cloudinary.com/dt3k2apqd/video/upload/f_webm/Loop%20Film/LoopFilm_ix0zjg.mp4"
           type="video/mp4"
         />
       </video>
