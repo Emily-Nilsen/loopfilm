72b359813c487526b0aeb8735bcd7702f73e8448
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2023-02-14 21:10:28 +0100

    Reinstall poster and video web format

--- src/components/BackgroundVideo.jsx
+++ src/components/BackgroundVideo.jsx
@@ -6,10 +6,10 @@ export function BackgroundVideo() {
         autoPlay
         muted
         playsInline
-        // poster="https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/Loop%20Film/bmw-large_rbr1c8.webp"
+        poster="https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/Loop%20Film/bmw-large_rbr1c8.webp"
       >
         <source
-          src="https://res.cloudinary.com/dt3k2apqd/video/upload/q_auto/Loop%20Film/LoopFilm_2_owtl3q.mp4"
+          src="https://res.cloudinary.com/dt3k2apqd/video/upload/f_webm/Loop%20Film/LoopFilm_2_owtl3q.mp4"
           type="video/mp4"
         />
       </video>
