efcb11eca592e039011e5daa4d30d39267d6aadd
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2023-03-11 21:00:46 +0100

    Add more photos to collage

--- src/components/LoopFilmDetails.jsx
+++ src/components/LoopFilmDetails.jsx
@@ -7,9 +7,9 @@ import { AboutInfographicRight } from './AboutInfographicRight'
 
 export function LoopFilmDetails() {
   return (
-    <div className="bg-t max-w-screen relative -mt-10 overflow-hidden bg-gradient-to-b from-gray-50 pt-16 pb-24 sm:pt-24 sm:pb-32 md:mt-0 lg:pb-40">
+    <div className="relative pt-16 pb-24 -mt-10 overflow-hidden bg-t max-w-screen bg-gradient-to-b from-gray-50 sm:pt-24 sm:pb-32 md:mt-0 lg:pb-40">
       <Container>
-        <div className="relative mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="relative px-6 mx-auto max-w-7xl lg:px-8">
           <div className="sm:text-center">
             <h2 className="text-lg font-semibold leading-8 text-fuchsia-600">
               Mountains to the Sea
@@ -17,14 +17,14 @@ export function LoopFilmDetails() {
             <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
               Extreme filmmaking
             </p>
-            <p className="mx-auto mt-6 max-w-2xl text-lg leading-8 text-gray-600">
+            <p className="max-w-2xl mx-auto mt-6 text-lg leading-8 text-gray-600">
               Whether you’re looking to shoot under extreme conditions or
               capture the beauty of our curvy, breathtaking roads and rugged
               coastline, we can help.
             </p>
           </div>
 
-          <div className="mt-16 flex justify-center sm:mx-auto sm:mt-20 sm:max-w-none">
+          <div className="flex justify-center mt-16 sm:mx-auto sm:mt-20 sm:max-w-none">
             <div className="grid grid-cols-1 gap-y-16 sm:grid-cols-3 sm:gap-y-10 sm:gap-x-0 lg:gap-y-6 lg:pr-20">
               <div className="sm:col-span-3 xl:col-span-2">
                 <AboutInfographicLeft
@@ -61,7 +61,7 @@ export function LoopFilmDetails() {
                   description="We’re also proud members of the Global Production Network, ensuring we are always up to date with the latest industry trends and standards."
                   bgColour="bg-gray-400/10"
                 />
-                <div className="mt-6 ml-3 flex sm:mt-0 sm:ml-10">
+                <div className="flex mt-6 ml-3 sm:mt-0 sm:ml-10">
                   <a
                     className="transition duration-300 ease-in-out hover:opacity-75"
                     target="_blank"
@@ -69,6 +69,7 @@ export function LoopFilmDetails() {
                     href="https://www.globalproductionnetwork.com/"
                   >
                     <Image
+                      alt="Global Production Network logo"
                       width={100}
                       height={30}
                       src="https://res.cloudinary.com/dt3k2apqd/image/upload/v1673762573/Loop%20Film/GPN_logo_wtwvlh.svg"
--- src/components/WorkGallery.jsx
+++ src/components/WorkGallery.jsx
@@ -11,6 +11,66 @@ const testimonials = [
         image:
           'https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/Loop%20Film/RA_bro2-kopi_stimib.webp',
         height: 'h-72',
+        rotation: 'sm:-rotate-3',
+        scale: 'sm:scale-110',
+      },
+      {
+        image:
+          'https://res.cloudinary.com/dt3k2apqd/image/upload/v1678559688/Loop%20Film/LoopFilm_img-25_eytw4c.webp',
+        height: 'h-60',
+        rotation: '-rotate-2',
+        scale: 'sm:scale-105',
+      },
+      {
+        image:
+          'https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/Loop%20Film/LoopFilm_img-28_u0zpq2.webp',
+        height: 'h-80',
+        rotation: 'rotate-2',
+        scale: 'sm:scale-110',
+      },
+      {
+        image:
+          'https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/Loop%20Film/LoopFilm_img-22_fl2jiv.webp',
+        height: 'h-56',
+        rotation: '-rotate-3',
+        scale: 'sm:scale-105',
+      },
+      {
+        image:
+          'https://res.cloudinary.com/dt3k2apqd/image/upload/v1678559687/Loop%20Film/LoopFilm_img-11_yz81f0.webp',
+        height: 'h-56',
+        rotation: 'rotate-2',
+        scale: 'sm:scale-110',
+      },
+    ],
+    [
+      {
+        image:
+          'https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/Loop%20Film/LoopFilm_img-02_hsq98i.webp',
+        height: 'h-60',
+        rotation: '-rotate-3',
+        scale: 'sm:scale-125',
+      },
+      {
+        image:
+          'https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/Loop%20Film/LoopFilm_img-26_so0lqd.webp',
+        height: 'h-80',
+        rotation: 'rotate-3',
+        scale: 'sm:scale-110',
+      },
+      {
+        image:
+          'https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/Loop%20Film/LoopFilm_img-21_onmk4l.webp',
+        height: 'h-60',
+        rotation: 'rotate-1',
+        scale: 'sm:scale-100',
+      },
+      {
+        image:
+          'https://res.cloudinary.com/dt3k2apqd/image/upload/v1678559687/Loop%20Film/LoopFilm_img-17_hcctfo.webp',
+        height: 'h-52',
+        rotation: '-rotate-2',
+        scale: 'sm:scale-125',
       },
     ],
   ],
@@ -18,8 +78,68 @@ const testimonials = [
     [
       {
         image:
+          'https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/Loop%20Film/LoopFilm_img-01_mc5xeq.webp',
+        height: 'h-72',
+        rotation: 'rotate-3',
+        scale: 'sm:scale-110',
+      },
+      {
+        image:
+          'https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/Loop%20Film/LoopFilm_img-24_i1fjnf.webp',
+        height: 'h-56',
+        rotation: '-rotate-3',
+        scale: 'sm:scale-110',
+      },
+      {
+        image:
+          'https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/Loop%20Film/LoopFilm_img-27_dht1rp.webp',
+        height: 'h-52',
+        rotation: 'rotate-3',
+        scale: 'sm:scale-125',
+      },
+      {
+        image:
+          'https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/Loop%20Film/LoopFilm_img-16_vtztxu.webp',
+        height: 'h-56',
+        rotation: '-rotate-2',
+        scale: 'sm:scale-110',
+      },
+    ],
+    [
+      {
+        image:
           'https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/Loop%20Film/workGallery-07_qilvqo.webp',
         height: 'h-64',
+        rotation: 'rotate-3',
+        scale: 'sm:scale-100',
+      },
+      {
+        image:
+          'https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/Loop%20Film/LoopFilm_img-29_p2wkgy.webp',
+        height: 'h-52',
+        rotation: '-rotate-2',
+        scale: 'sm:scale-125',
+      },
+      {
+        image:
+          'https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/Loop%20Film/LoopFilm_img-23_qfrxrm.webp',
+        height: 'h-96',
+        rotation: 'rotate-2',
+        scale: 'sm:scale-110',
+      },
+      {
+        image:
+          'https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/Loop%20Film/LoopFilm_img-19_nnkxgo.webp',
+        height: 'h-56',
+        rotation: '-rotate-3',
+        scale: 'sm:scale-105',
+      },
+      {
+        image:
+          'https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/Loop%20Film/LoopFilm_img-10_sbw3nb.webp',
+        height: 'h-80',
+        rotation: 'rotate-2',
+        scale: 'sm:scale-100',
       },
     ],
   ],
@@ -31,95 +151,99 @@ function classNames(...classes) {
 
 export function WorkGallery() {
   return (
-    <div className="relative pb-6 bg-white isolate pt-28 sm:pb-16 sm:pt-32">
-      <div className="absolute inset-x-0 overflow-hidden -translate-y-1/2 top-1/2 -z-10 transform-gpu opacity-30 blur-3xl">
-        <svg
-          viewBox="0 0 1313 771"
-          aria-hidden="true"
-          className="ml-[max(50%,38rem)] w-[82.0625rem]"
-        >
-          <path
-            id="bc169a03-3518-42d4-ab1e-d3eadac65edc"
-            fill="url(#85a0eca5-25f1-4ab9-af84-4e2d8d9cdbf3)"
-            d="M360.508 589.796 231.671 770.522 0 498.159l360.508 91.637 232.034-325.485c1.485 150.396 51.235 393.965 238.354 165.069C1064.79 143.261 1002.42-107.094 1171.72 46.97c135.44 123.252 148.51 335.641 138.11 426.428L971.677 339.803l24.062 411.461-635.231-161.468Z"
-          />
-          <defs>
-            <linearGradient
-              id="85a0eca5-25f1-4ab9-af84-4e2d8d9cdbf3"
-              x1="1313.17"
-              x2="-88.881"
-              y1=".201"
-              y2="539.417"
-              gradientUnits="userSpaceOnUse"
-            >
-              <stop stopColor="#f5d0fe" />
-              <stop offset={1} stopColor="#f0abfc" />
-            </linearGradient>
-          </defs>
-        </svg>
-      </div>
-      <div className="absolute inset-x-0 top-0 flex pt-32 overflow-hidden opacity-25 -z-10 transform-gpu blur-3xl sm:pt-40 xl:justify-end">
-        <svg
-          viewBox="0 0 1313 771"
-          aria-hidden="true"
-          className="ml-[-22rem] w-[82.0625rem] flex-none origin-top-right rotate-[30deg] xl:ml-0 xl:mr-[calc(50%-12rem)]"
-        >
-          <use href="#bc169a03-3518-42d4-ab1e-d3eadac65edc" />
-        </svg>
-      </div>
-      <div className="px-6 mx-auto -mt-24 max-w-7xl sm:-mt-16 lg:px-8">
-        <div className="grid max-w-2xl grid-cols-1 grid-rows-1 gap-8 mx-auto mt-16 text-sm leading-6 text-gray-900 sm:mt-20 sm:grid-cols-2 xl:mx-0 xl:max-w-none xl:grid-flow-col xl:grid-cols-4">
-          <figure className="relative block col-span-1 h-96 sm:col-span-2 sm:rounded-2xl sm:bg-white sm:shadow-lg sm:ring-1 sm:ring-gray-900/5 xl:col-start-2 xl:row-end-1">
-            <div className="absolute inset-0">
-              <Image
-                className="object-cover overflow-hidden rounded-2xl"
-                fill
-                src={featuredTestimonial.image}
-                alt="Loop Film"
-              />
-            </div>
-          </figure>
-          {testimonials.map((columnGroup, columnGroupIdx) => (
-            <div
-              key={columnGroupIdx}
-              className="space-y-8 xl:contents xl:space-y-0"
-            >
-              {columnGroup.map((column, columnIdx) => (
-                <div
-                  key={columnIdx}
-                  className={classNames(
-                    (columnGroupIdx === 0 && columnIdx === 0) ||
-                      (columnGroupIdx === testimonials.length - 1 &&
-                        columnIdx === columnGroup.length - 1)
-                      ? 'xl:row-span-2'
-                      : 'xl:row-start-1',
-                    'space-y-8'
-                  )}
-                >
-                  {column.map((testimonial) => (
-                    <figure
-                      key={testimonial.image}
-                      className={classNames(
-                        'relative rounded-2xl bg-white p-6 shadow-lg ring-1 ring-gray-900/5',
-                        testimonial.height
-                      )}
-                    >
-                      <div className="absolute inset-0">
-                        <Image
-                          className="object-cover overflow-hidden rounded-2xl"
-                          fill
-                          src={testimonial.image}
-                          alt="Loop Film"
-                        />
-                      </div>
-                    </figure>
-                  ))}
-                </div>
-              ))}
-            </div>
-          ))}
+    <section className="w-full overflow-x-hidden">
+      <div className="relative pb-6 bg-white isolate pt-28 sm:pb-16 sm:pt-32">
+        <div className="absolute inset-x-0 overflow-hidden -translate-y-1/2 top-1/2 -z-10 transform-gpu opacity-30 blur-3xl">
+          <svg
+            viewBox="0 0 1313 771"
+            aria-hidden="true"
+            className="ml-[max(50%,38rem)] w-[82.0625rem]"
+          >
+            <path
+              id="bc169a03-3518-42d4-ab1e-d3eadac65edc"
+              fill="url(#85a0eca5-25f1-4ab9-af84-4e2d8d9cdbf3)"
+              d="M360.508 589.796 231.671 770.522 0 498.159l360.508 91.637 232.034-325.485c1.485 150.396 51.235 393.965 238.354 165.069C1064.79 143.261 1002.42-107.094 1171.72 46.97c135.44 123.252 148.51 335.641 138.11 426.428L971.677 339.803l24.062 411.461-635.231-161.468Z"
+            />
+            <defs>
+              <linearGradient
+                id="85a0eca5-25f1-4ab9-af84-4e2d8d9cdbf3"
+                x1="1313.17"
+                x2="-88.881"
+                y1=".201"
+                y2="539.417"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop stopColor="#f5d0fe" />
+                <stop offset={1} stopColor="#f0abfc" />
+              </linearGradient>
+            </defs>
+          </svg>
+        </div>
+        <div className="absolute inset-x-0 top-0 flex pt-32 overflow-hidden opacity-25 -z-10 transform-gpu blur-3xl sm:pt-40 xl:justify-end">
+          <svg
+            viewBox="0 0 1313 771"
+            aria-hidden="true"
+            className="ml-[-22rem] w-[82.0625rem] flex-none origin-top-right rotate-[30deg] xl:ml-0 xl:mr-[calc(50%-12rem)]"
+          >
+            <use href="#bc169a03-3518-42d4-ab1e-d3eadac65edc" />
+          </svg>
+        </div>
+        <div className="px-6 mx-auto -mt-24 max-w-7xl sm:-mt-16 lg:px-8">
+          <div className="grid max-w-2xl grid-cols-1 grid-rows-1 gap-8 mx-auto mt-16 text-sm leading-6 text-gray-900 sm:mt-20 sm:grid-cols-2 xl:mx-0 xl:max-w-none xl:grid-flow-col xl:grid-cols-4">
+            <figure className="relative hidden col-span-1 h-96 rotate-2 sm:col-span-2 sm:block sm:rounded-2xl sm:bg-white sm:shadow-lg sm:ring-1 sm:ring-gray-900/5 xl:col-start-2 xl:row-end-1">
+              <div className="absolute inset-0">
+                <Image
+                  className="object-cover overflow-hidden rounded-2xl"
+                  fill
+                  src={featuredTestimonial.image}
+                  alt="Loop Film"
+                />
+              </div>
+            </figure>
+            {testimonials.map((columnGroup, columnGroupIdx) => (
+              <div
+                key={columnGroupIdx}
+                className="space-y-8 xl:contents xl:space-y-0"
+              >
+                {columnGroup.map((column, columnIdx) => (
+                  <div
+                    key={columnIdx}
+                    className={classNames(
+                      (columnGroupIdx === 0 && columnIdx === 0) ||
+                        (columnGroupIdx === testimonials.length - 1 &&
+                          columnIdx === columnGroup.length - 1)
+                        ? 'xl:row-span-2'
+                        : 'xl:row-start-1',
+                      'space-y-0 sm:space-y-8'
+                    )}
+                  >
+                    {column.map((testimonial) => (
+                      <figure
+                        key={testimonial.image}
+                        className={classNames(
+                          'relative rounded-2xl bg-white p-6 shadow-lg ring-1 ring-gray-900/5 sm:-mt-0',
+                          testimonial.height,
+                          testimonial.rotation,
+                          testimonial.scale
+                        )}
+                      >
+                        <div className="absolute inset-0">
+                          <Image
+                            className="object-cover overflow-hidden rounded-2xl"
+                            fill
+                            src={testimonial.image}
+                            alt="Loop Film"
+                          />
+                        </div>
+                      </figure>
+                    ))}
+                  </div>
+                ))}
+              </div>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   )
 }
