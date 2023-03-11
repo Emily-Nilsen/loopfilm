ac43a05dc934f93f63cd81d4ca0da146c46082a5
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2023-03-11 18:21:06 +0100

    Add links to member logo cloud

--- src/components/MemberLogoCloud.jsx
+++ src/components/MemberLogoCloud.jsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 import { GlobalProductionLogo } from '@/components/Logos'
 import { IACPLogo } from '@/components/Logos'
@@ -13,18 +14,38 @@ export function MemberLogoCloud() {
           LoopFilm is proudly affiliated with the following organisations
         </p>
         <div className="mt-6 grid grid-cols-2 gap-0.5 md:grid-cols-4 lg:mt-8">
-          <div className="flex justify-center col-span-1 px-8 py-8 bg-white">
-            <GlobalProductionLogo className="h-12 max-h-12" />
-          </div>
-          <div className="flex justify-center col-span-1 px-8 py-8 bg-white">
-            <IACPLogo className="h-12 max-h-12 fill-gray-400" />
-          </div>
-          <div className="flex justify-center col-span-1 px-8 py-8 bg-white">
-            <GreenTheBidLogo className="h-12 max-h-12 fill-gray-400" />
-          </div>
-          <div className="flex justify-center col-span-1 px-8 py-8 bg-white">
-            <VirkeLogo className="h-12 max-h-12 fill-gray-400" />
-          </div>
+          <Link
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.globalproductionnetwork.com/"
+          >
+            <div className="flex justify-center col-span-1 px-8 py-8 bg-white">
+              <GlobalProductionLogo className="h-12 transition duration-300 ease-in-out max-h-12 fill-gray-400 hover:fill-sky-pink" />
+            </div>
+          </Link>
+          <Link target="_blank" rel="noreferrer" href="https://www.aicp.com/">
+            <div className="flex justify-center col-span-1 px-8 py-8 bg-white">
+              <IACPLogo className="h-12 transition duration-300 ease-in-out max-h-12 fill-gray-400 hover:fill-sky-pink" />
+            </div>
+          </Link>
+          <Link
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.greenthebid.earth/"
+          >
+            <div className="flex justify-center col-span-1 px-8 py-8 bg-white">
+              <GreenTheBidLogo className="h-12 transition duration-300 ease-in-out max-h-12 fill-gray-400 hover:fill-sky-pink" />
+            </div>
+          </Link>
+          <Link
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.virke.no/?gclid=Cj0KCQiA6rCgBhDVARIsAK1kGPI-Qb9tauzOPe-XcenTJVr8OS7uYZKoGyi6F3km45kjOwe-c4_rlMAaAqGxEALw_wcB"
+          >
+            <div className="flex justify-center col-span-1 px-8 py-8 bg-white">
+              <VirkeLogo className="h-12 transition duration-300 ease-in-out max-h-12 fill-gray-400 hover:fill-sky-pink" />
+            </div>
+          </Link>
         </div>
       </div>
     </div>
