1acc69355bc38028465d5bb7775318d2dc9cb825
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2023-03-16 11:09:44 +0100

    comment out api

--- src/pages/api/films.js
+++ src/pages/api/films.js
@@ -1,12 +1,12 @@
-const { Client } = require('@notionhq/client')
+// const { Client } = require('@notionhq/client')
 
-const notion = new Client({ auth: process.env.NOTION_API_KEY })
+// const notion = new Client({ auth: process.env.NOTION_API_KEY })
 
-export default async (req, res) => {
-  const response = await notion.databases.query({
-    database_id: process.env.NOTION_DATABASE_ID,
-  })
+// export default async (req, res) => {
+//   const response = await notion.databases.query({
+//     database_id: process.env.NOTION_DATABASE_ID,
+//   })
 
-  console.log(response)
-  res.status(200).json({ response })
-}
+//   console.log(response)
+//   res.status(200).json({ response })
+// }
