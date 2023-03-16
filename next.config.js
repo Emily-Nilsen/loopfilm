77f7a9cbedfc26a97390b4d0cd1481e1e0eb15bc
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2023-03-16 07:57:27 +0100

    Add redirects

--- next.config.js
+++ next.config.js
@@ -12,6 +12,55 @@ const nextConfig = {
     domains: ['res.cloudinary.com'],
     unoptimized: true,
   },
+  async redirects() {
+    return [
+      {
+        source: 'http://www.loopfilm.no/',
+        destination: 'https://loopfilm.no',
+        permanent: true,
+      },
+      {
+        source: 'https://loopfilm.no/locations/',
+        destination: 'https://loopfilm.no/locations',
+        permanent: true,
+      },
+      {
+        source: 'https://www.loopfilm.no/',
+        destination: 'https://loopfilm.no',
+        permanent: true,
+      },
+      {
+        source: 'http://loopfilm.no/',
+        destination: 'https://loopfilm.no',
+        permanent: true,
+      },
+      {
+        source: 'https://loopfilm.no/work/',
+        destination: 'https://loopfilm.no/work',
+        permanent: true,
+      },
+      {
+        source: 'https://loopfilm.no/work/dream-big/',
+        destination: 'https://loopfilm.no/work',
+        permanent: true,
+      },
+      {
+        source: 'https://loopfilm.no/saga-holidays/',
+        destination: 'https://loopfilm.no/work',
+        permanent: true,
+      },
+      {
+        source: 'https://loopfilm.no/ex-machina/',
+        destination: 'https://loopfilm.no/work',
+        permanent: true,
+      },
+      {
+        source: 'http://loopfilm.no/locations',
+        destination: 'https://loopfilm.no/locations',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
