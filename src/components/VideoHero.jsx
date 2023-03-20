1aba28b56317dc1e81ef619ff3c7569679d7c4d4
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2023-03-20 19:32:09 +0100

    Edit navbar on work page

--- src/components/VideoHero.jsx
+++ src/components/VideoHero.jsx
@@ -3,19 +3,22 @@ import { motion, useInView, useMotionValue, useAnimation } from 'framer-motion'
 
 export function VideoHero() {
   return (
-    <motion.div
-      animate={{ opacity: 1 }}
-      initial={{ opacity: 0 }}
-      transition={{
-        duration: 0.8,
-        type: 'fade',
-      }}
-      className="relative z-20 h-auto w-full bg-gray-900 pt-[56.25%]"
-    >
-      <div className="absolute inset-0">
-        <BackgroundVideo />
-      </div>
-      {/* <div className="absolute top-0 left-0 w-full h-40 bg-gradient-to-b from-gray-900 sm:h-60 lg:h-80" /> */}
-    </motion.div>
+    <>
+      {/* <div className="w-full h-24 bg-gray-900" /> */}
+      <motion.div
+        animate={{ opacity: 1 }}
+        initial={{ opacity: 0 }}
+        transition={{
+          duration: 0.8,
+          type: 'fade',
+        }}
+        className="relative z-20 h-auto w-full bg-gray-900 pt-[56.25%]"
+      >
+        <div className="absolute inset-0">
+          <BackgroundVideo />
+        </div>
+        <div className="absolute top-0 left-0 h-20 w-full bg-gradient-to-b from-gray-900 sm:h-40" />
+      </motion.div>
+    </>
   )
 }
