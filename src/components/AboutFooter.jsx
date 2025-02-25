3bf7da632f62c544841e7955d88c8ac3092961e9
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2025-02-25 13:23:53 +0300

    about footer

--- src/components/AboutFooter.jsx
+++ src/components/AboutFooter.jsx
@@ -1,17 +1,10 @@
 import Image from 'next/image'
 import { Footer } from './Footer'
-
 import { motion } from 'framer-motion'
-import { useInView } from 'react-intersection-observer'
 
 export function AboutFooter() {
-  const [ref, inView] = useInView({
-    threshold: 0.5, // adjust as needed
-    triggerOnce: true,
-  })
-
   return (
-    <section className="bg-t relative">
+    <section className="relative bg-t">
       <div className="flex flex-col">
         <div className="relative z-10">
           <Footer />
@@ -26,11 +19,11 @@ export function AboutFooter() {
           }}
         >
           <Image
-            src="https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/Loop%20Film/aboutFooterv2_la1yic.webp"
+            src="/media/aboutFooterv2_la1yic.webp"
             alt="Norway’s picturesque mountains as the perfect backdrop"
             width={3000}
             height={821}
-            className="-mt-20 object-cover sm:-mt-36 lg:-mt-60 xl:-mt-72"
+            className="object-cover -mt-20 sm:-mt-36 lg:-mt-60 xl:-mt-72"
           />
         </motion.div>
       </div>
