c95280df12a7afe436a7c8033ac555620bdb302b
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2025-02-25 12:42:59 +0300

    home footer

--- src/components/HomepageFooter.jsx
+++ src/components/HomepageFooter.jsx
@@ -1,15 +1,8 @@
 import Image from 'next/image'
 import { Footer } from './Footer'
-
 import { motion } from 'framer-motion'
-import { useInView } from 'react-intersection-observer'
 
 export function HomepageFooter() {
-  const [ref, inView] = useInView({
-    threshold: 0.5, // adjust as needed
-    triggerOnce: true,
-  })
-
   return (
     <section className="relative">
       <div className="flex flex-col">
@@ -26,11 +19,11 @@ export function HomepageFooter() {
           }}
         >
           <Image
-            src="https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/Loop%20Film/homeFooter_okr1c4.webp"
+            src="/media/homeFooter_okr1c4.webp"
             alt="Beautiful Norway - LoopFilm’s expert drone shots"
             width={3000}
             height={1993}
-            className="-mt-40 object-cover sm:-mt-80"
+            className="object-cover -mt-40 sm:-mt-80"
           />
         </motion.div>
       </div>
