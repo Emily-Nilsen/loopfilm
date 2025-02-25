bd9fdb4d192295f5c83cfe554452d48a458ba296
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2025-02-25 12:44:37 +0300

    loop film feature reel

--- src/components/HomepageFeature.jsx
+++ src/components/HomepageFeature.jsx
@@ -47,7 +47,7 @@ function FeatureHeading() {
 
   return (
     <div className="overflow-x-hidden bg-white">
-      <div className="mx-auto max-w-7xl px-6 pb-6 sm:pb-10 lg:px-8">
+      <div className="px-6 pb-6 mx-auto max-w-7xl sm:pb-10 lg:px-8">
         <motion.div
           ref={ref}
           whileInView={{ opacity: inView ? 1 : 0 }}
@@ -60,7 +60,7 @@ function FeatureHeading() {
               <p className="mt-1 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl lg:text-6xl">
                 Film in Norway’s stunning natural settings
               </p>
-              <p className="mx-auto mt-5 max-w-xl text-xl text-gray-500">
+              <p className="max-w-xl mx-auto mt-5 text-xl text-gray-500">
                 From fjords, snowy mountains, and forests to cities, explore the
                 diverse and spectacular locations available for your next
                 production.
@@ -88,12 +88,12 @@ export function HomepageFeature() {
         variants={container}
         initial={reelInView ? 'show' : 'hidden'}
         animate={reelInView ? 'show' : 'hidden'}
-        className="mx-auto flex max-w-full justify-end"
+        className="flex justify-end max-w-full mx-auto"
       >
         <motion.div variants={slideIn} className="relative sm:-mt-16 lg:-mt-20">
           <Image
             alt="LoopFilm Feature Reel"
-            src="https://res.cloudinary.com/dt3k2apqd/image/upload/v1673943616/Loop%20Film/LoopFilm_feature_reel_w1zliz.svg"
+            src="/media/LoopFilm_feature_reel_w1zliz.svg"
             width={1187}
             height={829}
             className="relative z-0 object-cover"
